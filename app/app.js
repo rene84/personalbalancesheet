e89eae41a6186bde89e42d64fbe711f89aa0cc4a
René Mulder <rene.mulder@everett.nl>
2016-01-03 14:06:41 +0100

    switch to angular xeditable, use grid

--- app/app.js
+++ app/app.js
@@ -1,16 +1,47 @@
-'use strict';
+angular.module('app', [ "ui.bootstrap", "xeditable" ])
 
-angular.module('app', [ 'ngRoute', 'ui.bootstrap']).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/'})
-}]).
+.run(function(editableOptions) {
+	editableOptions.theme = 'bs3';
+})
 
-controller('AssetController', function() {
-		this.asset = {
-			name : 'House',
-			value : 200000,
-			revenue : 3,
-			cost : 1,
-			growth : 4
-		};
+.controller('BalanceCtrl', function($scope) {
+	this.assets = [ {
+		id : 1,
+		name : "House",
+		value : 200000,
+		growth : 5,
+		revenue : 200,
+		cost : 100
+	}, {} ];
+	this.addAsset = function(asset) {
+		this.assets.push(asset);
+	};
+	$scope.saveAsset = function(asset, id) {
+		console.log(asset.name + "  " + id);
+	};
+
+})
+
+.controller('IncomeCtrl', function($scope) {
+	this.income = [ {
+		id : 1,
+		name : "Wage",
+		amount : 60000,
+	}, {} ];
+	
+	$scope.saveIncome = function(element, id) {
+		console.log(element.name + "  " + id);
+	};
+
+})
+
+.controller('ExpenseCtrl', function($scope) {
+	this.expenses = [ {
+		id : 1,
+		name : "Taxes",
+		amount : 20000,
+	}, {} ];
+	
+	
+
 });
